35f987cf4c17d07dceccea690b3366f02fb50f2f
SamAddy <addisonsamuel70@yahoo.com>
2022-12-25 08:43:42 -0500

    added tic tac toe and flatten list challenge

---
+++ Programming_Aptitude/flatten_a_list.py
@@ -0,0 +1,23 @@
+"""
+Write a function that takes a list of lists and flattens it into a one-dimensional list.
+
+Name your function flatten. It should take a single parameter and return a list.
+
+For example, calling:
+
+flatten([[1, 2], [3, 4]])
+
+Should return the list:
+
+[1, 2, 3, 4]
+"""
+def flatten(param):
+    results = []
+    for i in param:
+        for j in i:
+            results.append(j)
+
+    return results
+
+
+print(flatten([[1, 2], [3, 4]]))
