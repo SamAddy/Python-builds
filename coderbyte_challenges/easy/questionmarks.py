bc2df004e9cac900a37d8ec087682543ad53a0ad
SamAddy <addisonsamuel70@yahoo.com>
2023-01-05 00:58:45 +0200

    Update question mark challenge

---
+++ coderbyte_challenges/easy/questionmarks.py
@@ -0,0 +1,21 @@
+def questions_marks(strparam):
+    count_q = 0
+    a = 0
+    start_count = False
+    true_exist = False
+    for i in strparam:
+        if start_count and i == '?':
+            count_q += 1
+        if i.isdigit():
+            a += int(i)
+            if a==10 and count_q == 3:
+                true_exist = True
+            if start_count and a == 10 and count_q != 3:
+                return 'false'
+            a = int(i)
+            count_q = 0
+            start_count = True
+    return 'true' if true_exist else 'false'
+
+
+print(questions_marks("aa6?9"))
