0486b5720a611a0b7ee1ecef3c3aa3609d96c885
SamAddy <addisonsamuel70@yahoo.com>
2024-05-31 14:36:14 +0300

    Update solution for remove_element.py challenge

--- LeetCode_challenges/top_150/array_and_string/remove_element.py
+++ LeetCode_challenges/top_150/array_and_string/remove_element.py
@@ -67,9 +67,9 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         k = 0
 
-        for num in range(len(nums)):
-            if nums[num] != val:
-                nums[k] = nums[num]
+        for index in range(len(nums)):
+            if nums[index] != val:
+                nums[k] = nums[index]
                 k += 1
 
         return k
